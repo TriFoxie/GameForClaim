06d2aadf1a7e8d3d38e1fed392a10c5437e09cb3
jo449 <jo449@>
2026-02-28 15:47:58 +0000

    Added namespaces

--- Assets/Scripts/IDamageCalculator.cs
+++ Assets/Scripts/IDamageCalculator.cs
@@ -1,34 +1,38 @@
 ﻿using System;
 
-public interface IDamageCalculator
+namespace GameForClaim
 {
-    /// <summary>
-    /// Get the damage calculator or instantiate a new one if none exists
-    /// </summary>
-    /// <returns>Singleton DamageCalculator Object</returns>
-    IDamageCalculator GetInstance();
+    public interface IDamageCalculator
+    {
+        /// <summary>
+        /// Get the damage calculator or instantiate a new one if none exists
+        /// </summary>
+        /// <returns>Singleton DamageCalculator Object</returns>
+        IDamageCalculator GetInstance();
 
-    /// <summary>
-    /// Calculate the value of the damage caused
-    /// and adjust the relative damage states of the parameter objects
-    /// </summary>
-    /// <remarks>
-    /// The sum of the damage will be updated to reflect the financial losses after the damage. <br/>
-    /// The states of the damaged objects will be updated to reflect new damage levels
-    /// </remarks>
-    /// <param name="component1">The object colliding with <paramref name="component2"/></param>
-    /// <param name="component2">The object being collided into by <paramref name="component1"/></param>
-    public void CalculateDamage(IDamagableComponent component1, IDamagableComponent component2);
+        /// <summary>
+        /// Calculate the value of the damage caused
+        /// and adjust the relative damage states of the parameter objects
+        /// </summary>
+        /// <remarks>
+        /// The sum of the damage will be updated to reflect the financial losses after the damage. <br/>
+        /// The states of the damaged objects will be updated to reflect new damage levels
+        /// </remarks>
+        /// <param name="component1">The object colliding with <paramref name="component2"/></param>
+        /// <param name="component2">The object being collided into by <paramref name="component1"/></param>
+        public void CalculateDamage(IDamagableComponent component1, IDamagableComponent component2);
 
-    /// <summary>
-    /// Returns the sum of all financial loss since last damage reset
-    /// </summary>
-    /// <returns>The value of damage</returns>
-    public float GetValueOfCurrentDamage();
+        /// <summary>
+        /// Returns the sum of all financial loss since last damage reset
+        /// </summary>
+        /// <returns>The value of damage</returns>
+        public float GetValueOfCurrentDamage();
 
-    /// <summary>
-    /// The current sum of the finanical impact of damage will be reset. <br/>
-    /// Damage is still retained on an object by object basis.
-    /// </summary>
-    public void ResetValues();
+        /// <summary>
+        /// The current sum of the finanical impact of damage will be reset. <br/>
+        /// Damage is still retained on an object by object basis.
+        /// </summary>
+        public void ResetValues();
+    }
+    
 }

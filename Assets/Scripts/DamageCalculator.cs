e046b679018d58e3cb6746d0a84e35e1c18470a6
jo449 <jo449@>
2026-02-28 19:05:48 +0000

    Implemented Damage Calculator

--- Assets/Scripts/DamageCalculator.cs
+++ Assets/Scripts/DamageCalculator.cs
@@ -25,7 +25,21 @@ namespace GameForClaim
 
         public void CalculateDamage(IDamagableComponent component1, IDamagableComponent component2)
         {
-            int speed =  CalculateRelativeSpeed(component1, component2);
+            // Calculate the current value (for future use to compare pre-post damage values
+            float currentValue1 = CalculateCurrentObjectValue(component1);
+            float currentValue2 = CalculateCurrentObjectValue(component2);
+            
+            // Do damage
+            float relativeSpeed =  CalculateRelativeSpeed(component1, component2);
+            
+            int damageFactor = (int) Math.Ceiling((relativeSpeed * relativeSpeed)/35);
+            
+            component1.SetNewHealthLevel(component1.GetHealthLevel() / damageFactor);
+            component2.SetNewHealthLevel(component2.GetHealthLevel() / damageFactor);
+            
+            // Calculate the difference in value for both objects and add it to the currently damaged
+            _monetaryLoss += currentValue1 - CalculateCurrentObjectValue(component1);
+            _monetaryLoss += currentValue1 - CalculateCurrentObjectValue(component2);
         }
 
         public float GetValueOfCurrentDamage()
@@ -38,12 +52,18 @@ namespace GameForClaim
             _damageCalculator = new DamageCalculator();
         }
 
-        private int CalculateRelativeSpeed(IDamagableComponent component1, IDamagableComponent component2)
+        private float CalculateRelativeSpeed(IDamagableComponent component1, IDamagableComponent component2)
         {
             Vector2 motion1 = component1.GetMovement();
             Vector2 motion2 = component2.GetMovement();
 
-            Vector2 relativeMotion = Vector2.Add(motion1, motion2);
+            Vector2 relativeMotion = Vector2.Subtract(motion1, motion2);
+            return relativeMotion.Length();
+        }
+
+        private float CalculateCurrentObjectValue(IDamagableComponent component)
+        {
+            return component.GetUndamagedValue() * (float) component.GetHealthLevel();
         }
     }
 }

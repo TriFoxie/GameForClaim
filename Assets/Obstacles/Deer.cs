2459e895f373f07cc5ea6c2140484efe9afdcfe1
trifoxie <105915392+TriFoxie@users.noreply.github.com>
2026-03-01 02:13:20 +0000

    Deer implements IDamageableComponent

--- Assets/Obstacles/Deer.cs
+++ Assets/Obstacles/Deer.cs
@@ -1,17 +1,26 @@
 using System;
+using GameForClaim;
 using UnityEngine;
+using Vector2 = System.Numerics.Vector2;
 
-public class Deer : MonoBehaviour
+public class Deer : MonoBehaviour, IDamagableComponent
 {
     //Deer defaults to looking at the player and moving towards it slowly.
     //Speed and rotation can be incremented using IncrementSpeed/Rotation(amount)
     
+    [Header("Damageable Component")]
+    [SerializeField] private float value = 0; //Cost of the car in GBP.
+    [Range(0, 1)] [SerializeField] private float health = 1; //How much damage the car can withstand remaining. 0 is broken 1 is fine.
+    [Range(0, 1)] [SerializeField] private float vulnerability = 0.1f; //How easily the car take damage. 0 is vulnerable 1 is strong.
+    
+    [Header("Configuration")]
     [SerializeField]
     private float speed = 1;
     [SerializeField]
     [Range(0,200)]
     private float collisionForceMultiplier = 1;//Wheeeeeee
-    public Transform player;
+    
+    private Transform player;
     private Rigidbody rb;
 
     public void SetSpeed(float speed)
@@ -33,6 +42,7 @@ public class Deer : MonoBehaviour
         transform.position += speed * Time.deltaTime * transform.forward;
     }
 
+    #region Unity Functions
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -52,4 +62,33 @@ public class Deer : MonoBehaviour
         
         rb.AddExplosionForce((other.rigidbody.mass * other.rigidbody.linearVelocity.magnitude + collisionSpeed * 200) * collisionForceMultiplier, other.transform.position, 20);
     }
+    #endregion
+
+    #region IDamageableComponent
+    public float GetUndamagedValue()
+    {
+        return value;
+    }
+
+    public Vector2 GetMovement()
+    {
+        var velocity3d = speed * transform.forward;
+        return new Vector2(velocity3d.x, velocity3d.z);
+    }
+
+    public decimal GetVunerability()
+    {
+        return (decimal)vulnerability;
+    }
+
+    public decimal GetHealthLevel()
+    {
+        return (decimal)health;
+    }
+
+    public void SetNewHealthLevel(decimal newHealthLevel)
+    {
+        health = (float)newHealthLevel;
+    }
+    #endregion
 }
